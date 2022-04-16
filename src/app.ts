9ac9d0506c1266d17f01cf5291b43b360b4538ab
arifpatanduk <arifpatanduk2@gmail.com>
2022-04-16 16:29:17 +0700

    connect to mongodb using mongoose

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 8080;
+
+const {
+  MONGODB_ATLAS_USERNAME,
+  MONGODB_ATLAS_PASSWORD,
+  MONGODB_ATLAS_DBNAME,
+} = process.env;
+const uri = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.q9m1d.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+mongoose.set("useFindAndModify", true);
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.info(`Example app listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    throw error;
+  });
